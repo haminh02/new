3fd5aafb655d93c9d14effa7a6d31285791a4b4d
Quang Minh Ha <haquangminhbnvn@gmail.com>
2022-04-27 17:24:19 +0700

    2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 let notes = [
     {
@@ -29,7 +33,25 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id == id)
+    response.status(204).end()
+    // if (note) {
+    //     response.json(note)
+    // } else {
+    //     response.status(404).end()
+    // }
+})
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
+
 const PORT = 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
